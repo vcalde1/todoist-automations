85c54d614b2a2ec993c62514a08c89f47838f581
Virginia <vcalde1@gmail.com>
2024-06-29 10:49:20 -0400

    Clear all task priorities and add high priority to tasks due today

--- daily/priorities.py
+++ daily/priorities.py
@@ -5,11 +5,16 @@ todoist = TodoistAPI('15140b93826821f4c92819d23fb7cf8ed6a4a859')
 
 todays_date = str(datetime.date.today())
 
+# Reset priorities for all tasks
+task_list = []
 for task in todoist.get_tasks():
+    task_list.append(task.id)
+    for x in task_list:
+        todoist.update_task(task_id=x, priority=1)
+
+    # Add high priority to all tasks due today
     tasks_dictionary = task.to_dict()
     if tasks_dictionary['due'] != None:
         if tasks_dictionary['due']['date'] == todays_date:
             task_id = task.id
             todoist.update_task(task_id=task_id, priority=4)
-            print('done')
-
